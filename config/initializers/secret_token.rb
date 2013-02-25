0093f8c4b170cea69abb76657431ef9af1baf59c
Luiz Gustavo <lglmoura@gmail.com>
2013-02-25 20:00:46 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obras::Application.config.secret_token = 'bc69ad2ae5da52c0b4bfa463d09af2219fefdcbce7693da6753940eab224b60e009a2e63fa31961cd10bb551e8f9d46cb8b30fb4493d67108ce01fb95465fed5'
